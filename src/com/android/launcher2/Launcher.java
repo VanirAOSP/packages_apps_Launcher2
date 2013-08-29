b6848b836058b058a412c2872966302cadfe50bc
PrimeDirective <activethrasher00@gmail.com>
2013-08-28 21:22:29 -0400

    stock launcher gradient fix.
    instead of setting the gradient with a drawable image that fades.. just set it to transparent
    
    Change-Id: I1801406b80a17c7e05de6d9ff58c926059edf798

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -983,7 +983,7 @@ public final class Launcher extends Activity
         mDockDivider = findViewById(R.id.dock_divider);
 
         mLauncherView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        mWorkspaceBackgroundDrawable = getResources().getDrawable(R.drawable.workspace_bg);
+        mWorkspaceBackgroundDrawable = new ColorDrawable(Color.TRANSPARENT); 
 
         // Setup the drag layer
         mDragLayer.setup(this, dragController);
